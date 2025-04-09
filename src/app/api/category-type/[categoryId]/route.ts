de8307ed0214176176ab436f8d86153937143b54
Mahin <mahinsabbirhusen@gmail.com>
2025-04-09 18:37:14 +0530

    fixed prod bug

--- src/app/api/category-type/[categoryId]/route.ts
+++ src/app/api/category-type/[categoryId]/route.ts
@@ -5,9 +5,9 @@ import { db } from '@/lib/db';
 
 export async function GET(
   req: Request,
-  context: { params: { categoryId: string } },
+  { params }: { params: { categoryId: string } },
 ) {
-  const { categoryId } = await context.params;
+  const { categoryId } = params;
 
   try {
     const [category] = await db.query<RowDataPacket[]>(
